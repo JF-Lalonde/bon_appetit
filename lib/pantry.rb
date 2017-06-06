17e15a1b666feb3080380cdbe3d8464766fa8360
JF-Lalonde <lalondej88@gmail.com>
2017-06-06 10:15:32 -0600

    passing iteration 1

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,26 @@
+require './lib/recipe'
+
 class Pantry
+  attr_accessor :stock,
+              :total
+
+  def initialize
+    @stock = Hash.new(0)
+    @total = Array.new
+  end
+
+  def stock_check(item)
+    @stock[item]
+  end
+
+  def quantity(amount)
+    @total << amount
+    sum = total.reduce(:+)
+  end
+
+  def restock(item, amount)
+    @stock[item] = quantity(amount)
+  end
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,41 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_pantry_exists
+    pantry = Pantry.new
+    actual = pantry
+
+    assert_instance_of Pantry, pantry
+  end
+  def test_stock_is_empty_hash
+    pantry = Pantry.new
+    actual = pantry.stock.class
+    assert_equal Hash, actual
+  end
+
+  def test_stock_check_starts_at_zero
+    pantry = Pantry.new
+    actual = pantry.stock_check("Cheese")
+
+    assert_equal 0, actual
+  end
+
+  def test_restock_adds_amount
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    actual = pantry.stock_check("Cheese")
+
+    assert_equal 10, actual
+  end
+
+  def test_values_add_up
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+    actual = pantry.stock_check("Cheese")
+
+    assert_equal 30, actual
+  end
+
+  
 end

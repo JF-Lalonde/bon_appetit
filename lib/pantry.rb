0e3411496eaaa12a753cf65c56c945c97d5f3337
JF-Lalonde <lalondej88@gmail.com>
2017-06-06 11:13:10 -0600

    refactoring round 1 for pantry

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -25,18 +25,26 @@ class Pantry
   end
 
   def convert_units(recipe)
-    result = recipe.ingredients.map do |name, amount|
+    recipe.ingredients.map do |name, amount|
       if amount > 100
-        new_amount = (amount / 100).to_i
-        @output[name] = {quantity: new_amount, units: "Centi-Units"}
+        to_centi_units(name, amount)
       elsif amount < 1
-        new_amount = (amount * 1000).to_i
-        @output[name] = {quantity: new_amount, units: "Milli-Units"}
+        to_milli_units(name, amount)
       else
         @output[name] = {quantity: amount, units: "Universal Units"}
       end
     end
     @output
+  end
+
+  def to_centi_units(name, amount)
+    new_amount = (amount / 100).to_i
+    @output[name] = {quantity: new_amount, units: "Centi-Units"}
+  end
+
+  def to_milli_units(name, amount)
+    new_amount = (amount * 1000).to_i
+    @output[name] = {quantity: new_amount, units: "Milli-Units"}
   end
 
 end
